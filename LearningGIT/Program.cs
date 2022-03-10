3a8ced546f4847d1469240574a4978311c562323
Mohammed Shelle <Mohammed.Shelle@fcmb.com>
2022-03-10 23:48:40 +0100

    loving

---
+++ LearningGIT/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LearningGIT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World! grarte");
+        }
+    }
+}
